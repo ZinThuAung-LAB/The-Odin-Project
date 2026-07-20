e841f8c4c251424118cded46eafd16f6e9f0865e
ZinThuAung <zinthyaung@gmail.com>
2026-07-20 16:58:51 +0630

    final update

--- Rock-Paper-Scissor/app.js
+++ Rock-Paper-Scissor/app.js
@@ -1,1 +1,58 @@
-console.log("Hello World");
+function getComputerChoice() {
+  let randomIndex = Math.floor(Math.random() * 3);
+  if (randomIndex < 1) {
+    return "rock";
+  } else if (randomIndex < 2) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+function getHumanChoice() {
+  let humanChoice = prompt("Enter your choice").toLowerCase();
+  return humanChoice;
+}
+
+function playGame() {
+  let computerScore = 0;
+  let humanScore = 0;
+  let computerChoice = getComputerChoice();
+  let humanChoice = getHumanChoice();
+  function playRound(computerChoice, humanChoice) {
+    if (
+      (computerChoice == "rock" && humanChoice == "scissors") ||
+      (computerChoice == "paper" && humanChoice == "rock") ||
+      (computerChoice == "scissors" && humanChoice == "paper")
+    ) {
+      computerScore++;
+      return `You Lose! ${computerChoice} beat  ${humanChoice}`;
+    } else if (computerChoice === humanChoice) {
+      return "Tie!";
+    } else {
+      humanScore++;
+      return "You Wins!";
+    }
+  }
+
+  while (computerScore < 5 && humanScore < 5) {
+    let computerChoice = getComputerChoice();
+    let humanChoice = getHumanChoice();
+
+    console.log(playRound(computerChoice, humanChoice));
+    console.log(
+      `Current score -> Player: ${humanScore} | Computer: ${computerScore}`,
+    );
+  }
+
+  if (humanScore === 5) {
+    console.log("PLayer is the Winner");
+  } else if (computerScore === 5) {
+    console.log("Computer is the winner");
+  }
+  console.log(
+    `Current Score -> Player: ${humanScore} | Computer: ${computerScore}`,
+  );
+}
+
+playGame();

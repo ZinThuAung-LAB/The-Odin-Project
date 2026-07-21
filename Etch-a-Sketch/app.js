813a869b32c73c4e5f0e8af9cf30153083b8b735
ZinThuAung <zinthyaung@gmail.com>
2026-07-21 16:38:28 +0630

    add comment for random background color code

--- Etch-a-Sketch/app.js
+++ Etch-a-Sketch/app.js
@@ -13,6 +13,13 @@ squaresInput.addEventListener("change", (e) => {
   }
 });
 
+// function getRandomColor() {
+//   const r = Math.floor(Math.random() * 256);
+//   const g = Math.floor(Math.random() * 256);
+//   const b = Math.floor(Math.random() * 256);
+//   return `rgb(${r}, ${g}, ${b})`;
+// }
+
 function draw(size) {
   gridContainer.innerHTML = "";
   const totalSquare = size * size;
